450e26952943f612e883f980981a9dd350557d15
klisly <wizardholy@163.com>
2019-11-03 01:43:08 +0800

    init repo

---
+++ common/utils.py
@@ -0,0 +1,135 @@
+import collections
+import math
+import os
+import random
+import sys
+import tarfile
+import time
+import zipfile
+
+from IPython import display
+from matplotlib import pyplot as plt
+import mxnet as mx
+from mxnet import autograd, gluon, image, init, nd
+from mxnet.contrib import text
+from mxnet.gluon import data as gdata, loss as gloss, nn, utils as gutils
+import numpy as np
+
+def use_svg_display():
+    """Use svg format to display plot in jupyter"""
+    display.set_matplotlib_formats('svg')
+
+def show_fashion_mnist(images, labels):
+    """Plot Fashion-MNIST images with labels."""
+    use_svg_display()
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.reshape((28, 28)).asnumpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set matplotlib figure size."""
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+def data_iter(batch_size, features, labels):
+    """Iterate through a data set."""
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        j = nd.array(indices[i: min(i + batch_size, num_examples)])
+        yield features.take(j), labels.take(j)
+        
+def linreg(X, w, b):
+    """Linear regression."""
+    return nd.dot(X, w) + b
+
+
+def load_data_fashion_mnist(batch_size, resize=None, root=os.path.join(
+        '~', '.mxnet', 'datasets', 'fashion-mnist')):
+    """Download the fashion mnist dataset and then load into memory."""
+    root = os.path.expanduser(root)
+    transformer = []
+    if resize:
+        transformer += [gdata.vision.transforms.Resize(resize)]
+    transformer += [gdata.vision.transforms.ToTensor()]
+    transformer = gdata.vision.transforms.Compose(transformer)
+
+    mnist_train = gdata.vision.FashionMNIST(root=root, train=True)
+    mnist_test = gdata.vision.FashionMNIST(root=root, train=False)
+    num_workers = 0 if sys.platform.startswith('win32') else 4
+
+    train_iter = gdata.DataLoader(mnist_train.transform_first(transformer),
+                                  batch_size, shuffle=True,
+                                  num_workers=num_workers)
+    test_iter = gdata.DataLoader(mnist_test.transform_first(transformer),
+                                 batch_size, shuffle=False,
+                                 num_workers=num_workers)
+    return train_iter, test_iter
+
+def cross_entropy(y_hat, y):
+    return - nd.pick(y_hat, y).log()
+
+def evaluate_accuracy(data_iter, net):
+    accsum, n = 0.0, 0
+    for X, y in data_iter:
+        y = y.astype('float32')
+        accsum += (net(X).argmax(axis=1) == y).sum().asscalar()
+        n += y.size
+    return accsum / n
+
+
+def get_fashion_mnist_labels(labels):
+    """Get text label for fashion mnist."""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def accuracy(y_hat, y):
+    return (y_hat.argmax(axis = 1) == y.astype('float32')).mean().asscalar()
+
+def sgd(params, lr, batch_size):
+    for param in params:
+        param[:] = param - lr * param.grad / batch_size
+
+def squared_loss(y_hat, y):
+    """Squared loss."""
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+def l2_penalty(w):
+    return (w ** 2).sum()/2
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params = None, lr = None, trainer = None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            with autograd.record():
+                y_hat = net(X)
+                l = loss(y_hat, y).sum()
+            l.backward()
+            if trainer is None:
+                sgd(params, lr, batch_size)
+            else:
+                trainer.step(batch_size)  # “softmax回归的简洁实现”一节将用到
+            y = y.astype('float32')
+            train_l_sum += l.asscalar()
+            train_acc_sum += (y_hat.argmax(axis=1) == y).sum().asscalar()
+            n += y.size
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+        
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    """Plot x and log(y)."""
+    set_figsize(figsize)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        plt.legend(legend)
+    plt.show()
